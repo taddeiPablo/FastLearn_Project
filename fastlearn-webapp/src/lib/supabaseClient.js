2fa16f6ee3d5493aa12a26bafa79e3e76223e8e2
pablo <taddeiApablo23@gmail.com>
2025-09-11 09:23:47 -0300

    aplicando cambio
    aplicando un cambio en la capa de conexion hacia supabase

--- fastlearn-webapp/src/lib/supabaseClient.js
+++ fastlearn-webapp/src/lib/supabaseClient.js
@@ -1,7 +1,3 @@
 import { createClient } from '@supabase/supabase-js';
 
-/*const fastlearnWebappUrl = process.env.fastlearnWebappUrl;
-console.log(fastlearnWebappUrl);
-const fastlearnAnonKey = process.env.fastlearnAnonKey;
-*/
 export const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_ANON_KEY);
